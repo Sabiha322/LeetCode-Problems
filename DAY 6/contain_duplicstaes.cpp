371e69c1d58a1c5be2e3a3db5e8919c00bd2ac36
Sabiha <sksabiha322@gmail.com>
2025-04-16 13:31:18 +0530

    Add Single Number &Contains Duplicate
    Single number :(Brute Force & Optimized XOR approach)
    Contains Duplicate :Brute Force, Sorted, HashSet approaches

---
+++ DAY 6/contain_duplicstaes.cpp
@@ -0,0 +1,61 @@
+﻿// Problem: Contains Duplicate (LeetCode 217)
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/contains-duplicate/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/////////////////////
+// Brute Force Approach – O(n^2) time
+/////////////////////
+bool containsDuplicateBruteForce(vector<int>& nums) {
+    int n = nums.size();
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(nums[i] == nums[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+/////////////////////
+// Sorted Approach – O(n log n) time, O(1) space
+/////////////////////
+bool containsDuplicateSorted(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    for(int i = 0; i < nums.size() - 1; i++) {
+        if(nums[i] == nums[i + 1]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/////////////////////
+// HashSet Approach – O(n) time, O(n) space
+/////////////////////
+bool containsDuplicateHashSet(vector<int>& nums) {
+    unordered_set<int> seen;
+    for(int num : nums) {
+        if(seen.count(num)) {
+            return true;
+        }
+        seen.insert(num);
+    }
+    return false;
+}
+
+/////////////////////
+// Test code (can be removed if submitting to LeetCode)
+/////////////////////
+int main() {
+    vector<int> nums = {1, 2, 3, 4, 1};
+
+    cout << "Brute Force Result: " << containsDuplicateBruteForce(nums) << endl;
+    cout << "Sorted Result: " << containsDuplicateSorted(nums) << endl;
+    cout << "HashSet Result: " << containsDuplicateHashSet(nums) << endl;
+
+    return 0;
+}
---
+++ DAY 6/single_number.cpp
@@ -0,0 +1,47 @@
+﻿// Problem: Single Number (LeetCode 136)
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/single-number/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/////////////////////
+// Brute Force Approach – O(n^2) time
+/////////////////////
+int singleNumberBruteForce(vector<int>& nums) {
+    for(int i = 0; i < nums.size(); i++) {
+        int count = 0;
+        for(int j = 0; j < nums.size(); j++) {
+            if(nums[i] == nums[j]) {
+                count++;
+            }
+        }
+        if(count == 1) {
+            return nums[i];
+        }
+    }
+    return -1; // should never hit this case
+}
+
+/////////////////////
+// Optimized XOR Approach – O(n) time, O(1) space
+/////////////////////
+int singleNumberXOR(vector<int>& nums) {
+    int result = 0;
+    for(int num : nums) {
+        result ^= num;  // XOR cancels out duplicates
+    }
+    return result;
+}
+
+/////////////////////
+// Test code (can be removed if submitting to LeetCode)
+/////////////////////
+int main() {
+    vector<int> nums = {4, 1, 2, 1, 2};
+
+    cout << "Brute Force Result: " << singleNumberBruteForce(nums) << endl;
+    cout << "Optimized XOR Result: " << singleNumberXOR(nums) << endl;
+
+    return 0;
+}
