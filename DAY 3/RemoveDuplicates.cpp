910d06956652069d8a5bf4fde141c6fa89369c62
Sabiha <sksabiha322@gmail.com>
2025-04-16 13:40:55 +0530

    "Add solution for Longest Common Prefix"  & "Solved Remove Duplicates from Sorted Array"

---
+++ DAY 3/RemoveDuplicates.cpp
@@ -0,0 +1,21 @@
+﻿// Problem: Remove Duplicates from Sorted Array
+// Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
+
+        int i = 0;
+        for (int j = 1; j < nums.size(); ++j) {
+            if (nums[j] != nums[i]) {
+                ++i;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+};
